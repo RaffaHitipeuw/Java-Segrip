b81b3d82d946924b34133dc37d839a616d94d8da
rafa <144977755+RaffaHitipeuw@users.noreply.github.com>
2025-01-28 14:15:32 +0700

    Tugas Sembilan

---
+++ M8/sembilan.js
@@ -0,0 +1,33 @@
+// Tugas Satu
+
+let nilaiMTK = 91;
+let nilaiIPA = 81;
+let nilaiIPS = 91;
+
+if (nilaiMTK >= 91 && nilaiIPA >= 91 && nilaiIPS >= 91) {
+  console.log("Andi lulus");
+} else {
+  console.log("Andi tidak lulus");
+}
+
+// Tugas Dua
+
+let nilaiMTKK = 91;
+let nilaiIPAA = 81;
+let nilaiIPSS = 91;
+
+if (nilaiMTKK >= 91 || nilaiIPAA >= 91 || nilaiIPSS >= 91) {
+  console.log("Andi lulus");
+} else {
+  console.log("Andi tidak lulus");
+}
+
+// Tugas Tiga
+
+let isLoggedIn = true;
+
+if (isLoggedIn) {
+  console.log("Anda sudah login");
+} else {
+  console.log("Anda belum login");
+}
