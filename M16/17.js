002005664e0f2a18f113e3817fed3b7b604e8910
rafa <144977755+RaffaHitipeuw@users.noreply.github.com>
2025-02-20 11:34:29 +0700

    Tugas17

---
+++ M16/17.js
@@ -0,0 +1,32 @@
+// ! Latihan 1
+
+let umur = 25;
+console.log(umur);
+
+// ! Latihan 2
+"use strict";
+x = 10;
+console.log(x);
+
+// ! Latihan 3
+let data = "Halo";
+console.log(typeof data);
+
+data = 42;
+console.log(typeof data);
+
+data = true;
+console.log(typeof data);
+
+// ! Latihan 4
+let nama = prompt("masukkan nama kamu:");
+alert("hello " + nama + ", selamat datang di Pondok");
+
+// ! Latihan 5
+let lanjut = confirm("Apakah kamu ingin melanjutkan ke halaman lain?");
+
+if (lanjut) {
+  alert("Anda memilih untuk melanjutkan");
+} else {
+  alert("Anda membatalkan tindakan");
+}
